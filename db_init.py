5dbce56365c3dbc999a27f9ece55afaff9d16f61
daeseong kim <scvgoe@naver.com>
2016-09-25 06:21:45 +0000

    wsgi config

---
+++ db_init.py
@@ -0,0 +1,22 @@
+from kkuziri import app, db
+from kkuziri.models import *
+
+db.drop_all()
+db.create_all()
+
+admin = User("admin_id", "admin pw")
+db.session.add(admin)
+db.session.commit()
+
+category = Category("default")
+db.session.add(category)
+db.session.commit()
+
+post = Post("Hello, world!", "Hello, world!", admin.id, category.id)
+db.session.add(post)
+db.session.commit()
+
+comment = Comment("Bye, world!", admin.id, post.id)
+db.session.add(comment)
+db.session.commit()
+
