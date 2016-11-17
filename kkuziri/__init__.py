5e345023ee6c3e9e993c06597ce54f05b1a6f380
daeseong Kim <scvgoe@naver.com>
2016-11-17 16:05:22 +0900

    migration applied

--- kkuziri/__init__.py
+++ kkuziri/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager 
 
 import markdown
 
@@ -14,6 +16,10 @@ except:
 # db = SQLAlchemy(app, session_options={'autocommit': True})
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 # jinja2 function add
 # markdown fucntion call in jinja2
