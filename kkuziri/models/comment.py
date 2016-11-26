e6a7725dfa3ea3f309d15ef72678608eb42a077b
DaeseongKim <scvgoe@naver.com>
2016-11-26 23:59:23 +0900

    comment getter added

--- kkuziri/models/comment.py
+++ kkuziri/models/comment.py
@@ -5,7 +5,7 @@ from post import *
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     body = db.Column(db.Text)
-    author_id = db.Column(db.String(64))
+    author_id = db.Column(db.String(64), db.ForeignKey('user.id'))
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
     created_at = db.Column(db.DateTime)
     modified_at = db.Column(db.DateTime)
@@ -15,6 +15,27 @@ class Comment(db.Model):
         self.author_id = author_id
         self.post_id = post_id
         self.created_at = datetime.now()
+
+    def get_author(self):
+        return self.author
+
+    def get_author_id(self):
+        return self.author_id
+
+    def get_body(self):
+        return self.body
+
+    def get_created_at(self):
+        return self.created_at
+
+    def get_id(self):
+        return self.id
+
+    def get_modified_at(self):
+        return self.modified_at
+
+    def get_post_id(self):
+        return self.post_id
 
     @staticmethod
     def is_valid(body, author_id, post_id):
@@ -28,7 +49,7 @@ class Comment(db.Model):
 
     @staticmethod
     def new_comment(body, author_id, post_id):
-        if not is_valid(body, author_id, post_id):
+        if not Comment.is_valid(body, author_id, post_id):
             return None
 
         comment = Comment(body, author_id, post_id)
