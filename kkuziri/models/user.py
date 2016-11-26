983b98804231a632b8a1861648adf3f422b294a7
DaeseongKim <scvgoe@naver.com>
2016-11-27 00:00:06 +0900

    user getter added

--- kkuziri/models/user.py
+++ kkuziri/models/user.py
@@ -7,17 +7,28 @@ class User(db.Model):
     name = db.Column(db.String(128))
     _password = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    comments = db.relationship('Comment', backref='author', lazy='dynamic')
     created_at = db.Column(db.DateTime)
     
     def __init__(self, username):
         self.username = username
         self.created_at = datetime.now()
 
+
+    def get_comments(self):
+        return self.comments
+
+    def get_created_at(self):
+        return self.created_at
+
     def get_id(self):
         return self.id
 
-    def get_password(self):
-        return self._password
+    def get_name(self):
+        return self.name
+
+    def get_posts(self):
+        return self.posts
 
     @staticmethod
     def get_user(id=0, username=''):
@@ -31,6 +42,9 @@ class User(db.Model):
                 first()
 
         return None
+
+    def get_username(self):
+        return self.username
 
     def is_correct_password(self, plaintext):
         return bcrypt.check_password_hash(self._password, plaintext)
