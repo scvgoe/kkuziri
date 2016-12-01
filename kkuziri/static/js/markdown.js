d9019eeb5150bb69978ccc3d6d3fa160499aa3ae
DaeseongKim <scvgoe@naver.com>
2016-12-01 23:55:36 +0900

    markdown editor change

--- kkuziri/static/js/markdown.js
+++ kkuziri/static/js/markdown.js
@@ -1,2 +1,18 @@
-var text = $('.markdown').text();
-$('.markdown').html(markdown.toHTML(text));
+var simplemde = new SimpleMDE({
+	toolbar: [
+	"bold", "italic", "strikethrough", "|",
+	"heading", "heading-smaller", "heading-bigger", "|",
+	"code", "quote", "unordered-list", "ordered-list", "clean-block", "|",
+	"link", 
+	{
+		name: "image",
+		action: function customFunction(editor){
+			
+		},
+		className: "fa fa-image",
+		title: "Insert Image",
+	},
+	"table", "horizontal-rule", "|",
+	"preview", "side-by-side", "fullscreen", "guide"
+	]
+});
